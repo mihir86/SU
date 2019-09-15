6687936d258ae31a6730a70a4b1210992944a59b
Aryan <aryan.arora180@gmail.com>
2019-09-15 13:19:35 +0530

    ALL CHANGES

--- app/src/main/java/com/example/su/ProfessorAvailableFragment.java
+++ app/src/main/java/com/example/su/ProfessorAvailableFragment.java
@@ -1,7 +1,5 @@
 package com.example.su;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,8 +9,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.zip.Inflater;
 
 public class ProfessorAvailableFragment extends Fragment {
 
