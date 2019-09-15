3c8d698cd98f102f3488a4837daa540f188964a4
Aryan <aryan.arora180@gmail.com>
2019-09-15 12:24:21 +0530

    Commit for rebase

---
+++ app/src/main/java/com/example/su/ProfessorAvailableFragment.java
@@ -0,0 +1,29 @@
+package com.example.su;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class ProfessorAvailableFragment extends Fragment {
+
+	public ProfessorAvailableFragment() {}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_professor_available, container,false);
+
+
+		return rootView;
+	}
+}
