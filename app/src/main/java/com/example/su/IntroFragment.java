2f1d49b632a378536740110b984014528383f377
Aryan <aryan.arora180@gmail.com>
2020-02-27 05:45:23 +0530

    Changed sign in flow to use fragments with navigation controller

--- app/src/main/java/com/example/su/IntroFragment.java
+++ app/src/main/java/com/example/su/IntroFragment.java
@@ -1,55 +1,50 @@
 package com.example.su;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroFragment extends Fragment {
 
     FirebaseFirestore db;
     boolean userHasDetailsInDatabase;
     String email;
 
+    public IntroFragment() {
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_intro, container, false);
 
         userHasDetailsInDatabase = false;
         db = FirebaseFirestore.getInstance();
 
-        try {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //TODO: (bug fix) User returned is always null
-            email = user.getEmail();
-        } catch (NullPointerException e) {
-            Log.d("IntroActivity", "FirebaseUser is null");
-            e.printStackTrace();
-            email = "null@gmail.com";
-        }
-
-        Button nextButton = findViewById(R.id.intro_next_button);
+        Button nextButton = rootView.findViewById(R.id.intro_next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences preferences = getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE);
+                SharedPreferences preferences = getContext().getSharedPreferences(getString(R.string.shared_pref_key), Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putBoolean(getString(R.string.signed_in_before_key), true).apply();
 
                 db.collection(getString(R.string.firebase_database_students_collection))
-                        .whereEqualTo(getString(R.string.firebase_database_student_email_field), email)
+                        .whereEqualTo(getString(R.string.firebase_database_student_email_field), FirebaseAuth.getInstance().getCurrentUser().getEmail())
                         .get()
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
@@ -57,9 +52,9 @@ public class IntroActivity extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     userHasDetailsInDatabase = !task.getResult().isEmpty();
                                     if (userHasDetailsInDatabase) {
-                                        startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                                        startActivity(new Intent(getContext(), MainActivity.class));
                                     } else {
-                                        startActivity(new Intent(IntroActivity.this, EnterPersonalDetailsActivity.class));
+                                        //TODO: Open EnterPersonalDetailsFragment through navigation controller
                                     }
                                 }
                             }
@@ -67,5 +62,7 @@ public class IntroActivity extends AppCompatActivity {
             }
         });
 
+        return rootView;
     }
+
 }
--- app/src/main/java/com/example/su/SignInActivity.java
+++ app/src/main/java/com/example/su/SignInActivity.java
@@ -3,141 +3,35 @@ package com.example.su;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
 
-    SignInButton googleSignInButton;
-    FirebaseAuth mAuth;
-    private final static int RC_SIGN_IN = 2;
-    GoogleSignInClient mGoogleSignInClient;
-
     private boolean signedInBefore;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            if (signedInBefore)
-                startActivity(new Intent(SignInActivity.this, MainActivity.class));
-            else
-                startActivity(new Intent(SignInActivity.this, IntroActivity.class));
-
-        } else {
-            googleSignInButton.setVisibility(View.VISIBLE);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
         SharedPreferences preferences = getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE);
         signedInBefore = preferences.getBoolean(getString(R.string.signed_in_before_key), false);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        googleSignInButton = findViewById(R.id.google_sign_in_button);
-        googleSignInButton.setStyle(SignInButton.SIZE_WIDE, SignInButton.COLOR_LIGHT);
-        googleSignInButton.setVisibility(View.INVISIBLE);
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-                if (checkIfBITSEmailAndStoreDetails(account.getEmail())) {
-                    if (signedInBefore)
-                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                    else
-                        startActivity(new Intent(SignInActivity.this, IntroActivity.class));
-                } else {
-                    mGoogleSignInClient.revokeAccess();
-                    Snackbar.make(findViewById(R.id.sign_in_layout), getString(R.string.sign_in_use_bits_mail), Snackbar.LENGTH_LONG).show();
-                }
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                e.printStackTrace();
-                Log.d("SignInDebug", "message - " + e.getStatusCode());
-                Snackbar.make(findViewById(R.id.sign_in_layout), getString(R.string.sign_in_failed), Snackbar.LENGTH_LONG).show();
+        if (account != null) {
+            if (signedInBefore) {
+                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            } else {
+                Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(R.id.action_signInWithGoogleFragment_to_introFragment);
             }
         }
+
     }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            Snackbar.make(findViewById(R.id.sign_in_layout), getString(R.string.sign_in_failed), Snackbar.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    private boolean checkIfBITSEmailAndStoreDetails(String email) {
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        if (email.toLowerCase().endsWith("@hyderabad.bits-pilani.ac.in")) {
-            if (email.toLowerCase().startsWith("f20")) {
-                editor.putString(getString(R.string.student_or_prof_key), getString(R.string.student_value));
-                editor.apply();
-            } else {
-                editor.putString(getString(R.string.student_or_prof_key), getString(R.string.prof_value));
-                editor.apply();
-            }
-            return true;
-        } else if (email.equalsIgnoreCase("aryan.arora180@gmail.com") || email.equalsIgnoreCase("mstrmihir2@gmail.com") || email.equalsIgnoreCase("k.vineeth2000@gmail.com")||email.equalsIgnoreCase("abuch99@gmail.com") ) {
-            editor.putString(getString(R.string.student_or_prof_key), getString(R.string.prof_value));
-            editor.apply();
-            return true;
-        } else
-            return false;
-    }
 }
